7453665becf8a7a5eba6e8ce93b744167cd9e378
WouterMoedt <wouter.moedt@live.nl>
2019-07-12 16:32:27 +0200

    naming fix

--- efs/urls.py
+++ efs/urls.py
@@ -5,7 +5,7 @@ from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('efsportfolio.urls')),
+    path('', include('portfolio.urls')),
    url(r'^accounts/login/$', views.login, name='login'),
    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page': '/'}),
 ]
